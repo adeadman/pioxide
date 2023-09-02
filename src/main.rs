aac224d9fe1ba81378b295ee2ce2ccb8fc09e0f5
Aaron Deadman <aaron@introspection.net.au>
2023-09-02 01:17:56 +0100

    SSD1306 micro display hello world

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,10 @@ use rp_pico::hal;
 use rp_pico::hal::clocks::Clock;
 use hal::pac;
 
+use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+use fugit::RateExtU32;
+use core::fmt::Write;
+
 //use rp2040_hal::clocks::Clock;
 
 const XTAL_FREQ_HZ: u32 = 12_000_000u32;
@@ -45,22 +49,49 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
+    // configure two pins to be I2C rather than GPIO
+    let scl_pin = pins.gpio1.into_mode::<hal::gpio::FunctionI2C>();
+    let sda_pin = pins.gpio0.into_mode::<hal::gpio::FunctionI2C>();
+
+    // Create the I2C bus from the two pins
+    let i2c = hal::I2C::i2c0(
+        pac.I2C0,
+        sda_pin,
+        scl_pin,
+        400_u32.kHz(),
+        &mut pac.RESETS,
+        &clocks.system_clock,
+    );
+
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(
+        interface,
+        DisplaySize128x32,
+        DisplayRotation::Rotate0,
+    ).into_terminal_mode();
+
+    // clear the screen
+    display.init().unwrap();
+    display.clear().unwrap();
+
+    //let mut message: [u8; 13] = ['H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!'];
+    let message: [u8; 13] = *b"Hello, World!";
+
+    for c in message {
+        let _ = display.write_str(
+            unsafe {
+                core::str::from_utf8_unchecked(&[c])
+            }
+        );
+    }
+
     let mut led_pin = pins.led.into_push_pull_output();
-    let mut green_pin = pins.gpio18.into_push_pull_output();
-    let mut amber_pin = pins.gpio19.into_push_pull_output();
-    let mut red_pin = pins.gpio20.into_push_pull_output();
     loop {
         info!("on!");
         led_pin.set_high().unwrap();
-        green_pin.set_high().unwrap();
-        amber_pin.set_high().unwrap();
-        red_pin.set_high().unwrap();
         delay.delay_ms(500);
         info!("off!");
         led_pin.set_low().unwrap();
-        green_pin.set_low().unwrap();
-        amber_pin.set_low().unwrap();
-        red_pin.set_low().unwrap();
         delay.delay_ms(500);
     }
 }
